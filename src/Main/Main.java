1ac755b61756c15aaa77665d9d0f9265edc2ce07
LuchoDMD <lucio.mdq21@gmail.com>
2021-04-18 17:57:52 -0300

    Version Vanilla

---
+++ src/Main/Main.java
@@ -0,0 +1,12 @@
+package Main;
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+
+    }
+}
+
+/**Los paquetes Collects/Interface son respecticamente para almacenar las collectiones e interfaces. Las clases se
+ * almacenan dentro de la carpeta object*/
