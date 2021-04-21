455c5d248b08b8a312440484eb7fc0ada7432095
diego <n17_94@hotmail.com>
2021-04-20 23:22:43 -0300

    creacion de cliente

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -4,7 +4,7 @@ public class Main
 {
     public static void main(String[] args)
     {
-        /**creacion de la rama rDiego*/
+
     }
 }
 
---
+++ src/Objs/Persona.java
@@ -0,0 +1,27 @@
+package Objs;
+
+public  class Persona {
+    private String nombre;
+    private String apellido;
+    private  int telefono;
+    private String direccion;
+
+
+    public Persona(String nombre, String apellido, int telefono, String direccion) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.telefono = telefono;
+        this.direccion = direccion;
+    }
+
+
+    @Override
+    public String toString() {
+        final StringBuilder sb = new StringBuilder("");
+        sb.append("Nombre= ").append(nombre);
+        sb.append(", Apellido= ").append(apellido);
+        sb.append(", Telefono= ").append(telefono);
+        sb.append(", Direccion= ").append(direccion);
+        return sb.toString();
+    }
+}
