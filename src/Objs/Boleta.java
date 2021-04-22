b1924107cddc06a8522ff55f297d75e9ad4c97ea
LuchoDMD <lucio.mdq21@gmail.com>
2021-04-22 18:46:21 -0300

    Ultimo commit

--- src/Objs/Boleta.java
+++ src/Objs/Boleta.java
@@ -9,7 +9,6 @@ public class Boleta
     /**Formato dd/mm/aaaa*/
     private static final String retiro= LocalDate.now().format(DateTimeFormatter.ofLocalizedDate(FormatStyle.SHORT));
     private static final String devolucion= LocalDate.now().plusDays(7).format(DateTimeFormatter.ofLocalizedDate(FormatStyle.SHORT));
-    /**private */
     @Override
     public String toString()
     {
