22c5ff50f2519b1b395f1f27207c9555712835ea
ElDiegote <n17_94@hotmail.com>
2021-04-22 16:55:54 -0300

    eliminamos persona

--- src/Objs/Cliente.java
+++ src/Objs/Cliente.java
@@ -1,12 +1,30 @@
 package Objs;
 
+public  class Cliente {
+    private String nombre;
+    private String apellido;
+    private  int telefono;
+    private String direccion;
 
-public class Cliente extends Persona {
 
-
-    public Cliente(String nombre, String apellido, Integer telefono, String direccion) {
-        super(nombre, apellido, telefono, direccion);
+    public Cliente(String nombre, String apellido, int telefono, String direccion) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.telefono = telefono;
+        this.direccion = direccion;
     }
 
+    public String getNombre() {
+        return nombre;
+    }
 
+    @Override
+    public String toString() {
+        final StringBuilder sb = new StringBuilder("");
+        sb.append("Nombre= ").append(nombre);
+        sb.append(", Apellido= ").append(apellido);
+        sb.append(", Telefono= ").append(telefono);
+        sb.append(", Direccion= ").append(direccion);
+        return sb.toString();
+    }
 }
